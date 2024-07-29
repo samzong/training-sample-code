6a4ec4d570711b20a06d363bddba0e85ff551228
Samzong <samzong.lu@gmail.com>
2024-07-29 23:02:50 +0800

    Create tensorflow-distributed.py

---
+++ tensorflow/tensorflow-distributed.py
@@ -0,0 +1,54 @@
+import os
+import json
+import tensorflow as tf
+
+class SimpleModel(tf.keras.Model):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.fc = tf.keras.layers.Dense(1, input_shape=(10,))
+
+    def call(self, x):
+        return self.fc(x)
+
+def train():
+    # 打印环境信息
+    print(f"TensorFlow version: {tf.__version__}")
+    print(f"GPU available: {tf.test.is_gpu_available()}")
+    if tf.test.is_gpu_available():
+        print(f"GPU device count: {len(tf.config.list_physical_devices('GPU'))}")
+
+    # 获取分布式训练信息
+    tf_config = json.loads(os.environ.get('TF_CONFIG') or '{}')
+    task_type = tf_config.get('task', {}).get('type')
+    task_id = tf_config.get('task', {}).get('index')
+
+    print(f"Task type: {task_type}, Task ID: {task_id}")
+
+    # 设置分布式策略
+    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+    
+    with strategy.scope():
+        model = SimpleModel()
+        loss_fn = tf.keras.losses.MeanSquaredError()
+        optimizer = tf.keras.optimizers.SGD(learning_rate=0.001)
+
+    # 生成一些随机数据
+    data = tf.random.normal((100, 10))
+    labels = tf.random.normal((100, 1))
+
+    @tf.function
+    def train_step(inputs, labels):
+        with tf.GradientTape() as tape:
+            predictions = model(inputs)
+            loss = loss_fn(labels, predictions)
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        return loss
+
+    for epoch in range(10):
+        loss = train_step(data, labels)
+        if task_type == 'chief':
+            print(f'Epoch {epoch}, Loss: {loss.numpy():.4f}')
+
+if __name__ == '__main__':
+    train()
